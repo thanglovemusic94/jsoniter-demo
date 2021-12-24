69af6d3afb842de2f1988be12b100ac6bd83d9e6
tony <thangnv@mintpotvn.com>
2021-12-24 18:25:36 +0700

    first commit

---
+++ src/main/java/com/example/jsoniterdemo/JsoniterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoniterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoniterDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoniterDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsoniterdemo/JsoniterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsoniterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsoniterDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
